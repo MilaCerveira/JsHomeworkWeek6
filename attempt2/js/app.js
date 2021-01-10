b062affafd00e2e028850fab2beb63989b976270
milacerveira <milascerveira@gmail.com>
2021-01-10 17:58:43 +0000

    dom listener

--- attempt2/js/app.js
+++ attempt2/js/app.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    createButton();
-    // test console.log('Hello World'); check in console
-})
     
+    // test 
+    //console.log('Hello World'); check in console
+    //form event listener 
+        createButton();
+        const theForm = document.querySelector ('#new-item-form');
+        theForm.addEventListener ('submit', handleFormSubmit);
+        
+        const deleteButton = document.querySelector ('button');
+        deleteButton.addEventListener ('click', handleDeleteClick);
+        
+      })
+      
+      
+      
