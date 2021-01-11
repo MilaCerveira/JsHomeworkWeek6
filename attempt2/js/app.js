af526eb65ebba324c6ca0b3e9e9fa1eb6f721a58
milacerveira <milascerveira@gmail.com>
2021-01-11 08:32:00 +0000

    added js

--- attempt2/js/app.js
+++ attempt2/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // test 
     //console.log('Hello World'); check in console
     //form event listener 
-        createButton();
+        
         const theForm = document.querySelector ('#new-item-form');
         theForm.addEventListener ('submit', handleFormSubmit);
         
@@ -13,4 +13,33 @@ document.addEventListener('DOMContentLoaded', () => {
       })
       
       
+      const handleFormSubmit = function (submission) {
+        submission.preventDefault();                                      
+        const newListItem = document.createElement('li');
+        newListItem.textContent = 
+        `${submission.target.title.value}
+        ${submission.target.artist.value}
+        ${submission.target.genre.value}`
       
+        const banana = document.querySelector('ul');
+        banana.appendChild(newListItem);
+        document.getElementById('new-item-form').reset();
+      }
+      
+      const handleDeleteClick = function () {
+        const resultUl = document.querySelector('ul');
+        resultUl.removeChild = '${target.title.value}'
+      }
+      
+      const handledeleteClick = function () {
+        const resultUl = document.querySelector('ul');
+        result.innerHTML = ''
+      }
+        
+      }
+
+      /*function createButton () {
+       const newButton = document.createElement ('button');
+       newButton.innerHTML = 'Delete All';
+       document.body.appendChild(newButton);
+      } */
